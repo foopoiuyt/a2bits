250304804f2628b47721143dbcc666cc6803fb75
Stephan Szabo <sszabo@zombiestormtrooper.com>
2022-04-16 16:25:06 -0700

    Move debug text routines to macros

--- test.system.asm
+++ test.system.asm
@@ -7,6 +7,7 @@
  copy constants/zp.asm
 
  mcopy routines/waitforkey.macros
+ mcopy routines/debugtext.macros
 
 main start
 
@@ -19,96 +20,13 @@ main start
 showerr anop
  lda TEXTON
 
-; Clear the text screen
- ldx #$04
- lda #$A0
-eclol anop
- ldy #$00
-eclil anop
- sta $400,Y
- iny
- bne eclil
- inc eclil+2
- dex
- bne eclol
+ cleartextscreen $A0
 
 ; Line 1: filename label and filename
- ldy ERRSTRFNAME
- ldx $00
-efl anop
- lda ERRSTRFNAME+1,X
- sta $400,X
- inx
- dey
- bne efl
+ writedebugstringline $400,ERRSTRFNAME,FNAME
 
-; Move the position for the filename forward by the 
-; number of characters for label
- txa
- adc efnp+1
- sta efnp+1
-
- ldy FNAME
- ldx $00
-efnl anop
- lda FNAME+1,X
-efnp anop
- sta $400,X ; Note: the lower byte here will be modified above
- inx
- dey
- bne efnl
-
-; Line 2 error code label and code
- ldy ERRSTRECODE
- ldx $00
-ecl anop
- lda ERRSTRECODE+1,X
- sta $480,X
- inx
- dey
- bne ecl
-
- lda ZPLOADBINERRORCODESTORE
- lsr A
- lsr A
- lsr A
- lsr A
- tay
- lda ERRSTRHEX,Y
- sta $480,X
- inx
-
- lda ZPLOADBINERRORCODESTORE
- and #$0F
- tay
- lda ERRSTRHEX,Y
- sta $480,X
-
-; Line 3 Error command (mli command that failed)
- ldy ERRSTRECMD
- ldx $00
-ecml anop
- lda ERRSTRECMD+1,X
- sta $500,X
- inx
- dey
- bne ecml
-
- lda ZPLOADBINERRORCMDSTORE
- lsr A
- lsr A
- lsr A
- lsr A
- tay
- lda ERRSTRHEX,Y
- sta $500,X
- inx
-
- lda ZPLOADBINERRORCMDSTORE
- and #$0F
- tay
- lda ERRSTRHEX,Y
- sta $500,X
+ writedebughexline $480,ERRSTRECODE,ZPLOADBINERRORCODESTORE,ERRSTRHEX
+ writedebughexline $500,ERRSTRECMD,ZPLOADBINERRORCMDSTORE,ERRSTRHEX
 
  waitforkey
 
